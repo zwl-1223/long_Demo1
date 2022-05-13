bdd0c439d28d1ae424c8137e42c266b14c8808df
朱文龙 <15601996072@139.com>
2022-05-13 16:54:50 +0800

    初次提交1

--- src/main/java/com/log/test/gitTest.java
+++ src/main/java/com/log/test/gitTest.java
@@ -3,5 +3,6 @@ package com.log.test;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("11111111111");
     }
 }
